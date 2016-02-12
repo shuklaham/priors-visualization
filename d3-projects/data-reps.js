831a27e1c0198706d41b7bee016ea4d2790393f9
shubham <shubham_shkl@nsitonline.in>
2016-02-12 01:44:43 -0500

    d2 attempt

---
+++ d3-projects/data-reps.js
@@ -0,0 +1,14 @@
+
+var findings = [
+"Hepatic Lesions",
+"Lymphadenopathy",
+"Adrenal Nodules",
+"Retroperitoneal Edema",
+"Small Volume Ascites",
+"Soft Tissue Thickening",
+"Vertebral Body",
+"Metastasis",
+"Hepatic Metastases",
+"Adrenal Metastasis"]
+
+var rep_one = 
